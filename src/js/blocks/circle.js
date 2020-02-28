8a080c727c9e58f86842308d4e8250ed0910dcb7
Elena <elena.do1988@yandex.ru>
2020-02-28 18:51:07 +0300

    circle js is refreshed

--- src/js/blocks/circle.js
+++ src/js/blocks/circle.js
@@ -1,29 +1,29 @@
 var radius = 100;
 var circles = document.querySelectorAll(".circle__item");
-var total_circles = circles.length;
+var totalСircles = circles.length;
 
 /* Set radius for all circles */
-for (var i = 0; i < total_circles; i++) {
+for (var i = 0; i < totalСircles; i++) {
 	circles[i].setAttribute('r', radius);
 }
 
-/* Set meter's wrapper dimension */
+/* Set circle's wrapper dimension */
 var wrapper = document.querySelector(".circle--wrapper");
 wrapper.style.width = "256px";
 wrapper.style.height = "296px";
 
 /* Add strokes to circle-mask  */
-var cf = 2 * Math.PI * radius;
-var semi_cf = cf / 2;
+var circleForm = 2 * Math.PI * radius;
+var semicircleForm = circleForm / 2;
 
 document.querySelector(".circle__item-mask")
-	.setAttribute("stroke-dasharray", semi_cf + "," + cf);
+	.setAttribute("stroke-dasharray", semicircleForm + "," + circleForm);
 
 /* Bind range slider event*/
-var mask = document.querySelector(".circle__item-mask");
-var meter_needle =  document.querySelector(".circle__needle");
 var slider = document.querySelector(".circle__slider");
 var sliderNumber = document.querySelector(".circle__slider-number");
+var mask = document.querySelector(".circle__item-mask");
+var circleNeedle =  document.querySelector(".circle__needle");
 var intervalCount;
 var startCount = slider.min;
 var endCount = 500;
@@ -33,11 +33,11 @@ var maxCount = slider.max;
 var countUp = function() {
 	currentCount++;
 
-	var meter_value = semi_cf - ((currentCount * semi_cf) / maxCount);
-	mask.setAttribute("stroke-dasharray", meter_value + "," + cf);
+	var circleValue = semicircleForm - ((currentCount * semicircleForm) / maxCount);
+	mask.setAttribute("stroke-dasharray", circleValue + "," + circleForm);
 
 	slider.value = currentCount;
-	meter_needle.style.transform = "rotate(" +
+	circleNeedle.style.transform = "rotate(" +
 		(270 + ((currentCount * 180) / maxCount)) + "deg)";
 	sliderNumber.textContent = currentCount;
 
