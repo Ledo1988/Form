953babd702b1dd3f8b3d47c6e6b1dae60414b957
Elena <elena.do1988@yandex.ru>
2020-02-28 16:51:12 +0300

    circle js is started

--- src/js/blocks/circle.js
+++ src/js/blocks/circle.js
@@ -20,18 +20,37 @@ document.querySelector(".circle__item-mask")
 	.setAttribute("stroke-dasharray", semi_cf + "," + cf);
 
 /* Bind range slider event*/
-var slider = document.querySelector(".circle__slider");
-var sliderValue = document.querySelector(".circle__slider-number");
 var mask = document.querySelector(".circle__item-mask");
 var meter_needle =  document.querySelector(".circle__needle");
+var slider = document.querySelector(".circle__slider");
+var sliderNumber = document.querySelector(".circle__slider-number");
+var currentValue = 500;
+var maxValue = slider.max;
+var minValue = slider.min;
 
 function range_change_event() {
-	var currentValue = slider.value;
-	var maxValue = slider.max;
 	var meter_value = semi_cf - ((currentValue * semi_cf) / maxValue);
+
 	mask.setAttribute("stroke-dasharray", meter_value + "," + cf);
-	meter_needle.style.transform = "rotate(" +
-		(270 + ((currentValue * 180) / maxValue)) + "deg)";
-	sliderValue.textContent = currentValue;
+
+	for (var j = 0; j <= currentValue; j++) {
+			meter_needle.style.transform = "rotate(" +
+				(270 + ((j * 180) / maxValue)) + "deg)";
+			sliderNumber.textContent = j;
+	}
 }
-slider.addEventListener("input", range_change_event);
+//slider.addEventListener("input", range_change_event);
+
+/* Slider Animation */
+function range_move_event() {
+
+	for (var i = 0; i <= currentValue; i++) {
+		setInterval(function(){
+			slider.value = i++;
+			range_change_event();
+		}, 1000);
+
+	}
+}
+
+window.onload = range_move_event;
